ae662df7dc7e925232ce43b305cd3463bb7feef3
laracrofTz <advaitaa.kat10@gmail.com>
2023-12-05 19:51:12 +0800

    Added stack qns folder

---
+++ stack_qns/valid_parentheses.py
@@ -0,0 +1,60 @@
+# Given string containing just the characters: (, ), {, }, [, ] 
+# String is only valid if the open bracket is closed by the same corresponding closed bracket and
+# the brackets must be open and closed in the correct order
+# Clarifications:
+    # The string only consists of the parenthesis characters? Not any other characters?
+        # Yes only the different parenthesis
+    # What is the maximum length of the string?
+        # 10^4
+    # Can the string start with a closed bracket?
+        # If it can start with a closed bracket, it is not a valid input string, since it violates the order of the brackets
+
+# Possible corner, edge cases would be
+# Null string, string with no parentheses, string with just whitespaces
+
+# using stack data structure
+from queue import LifoQueue
+
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        parentheses = {
+            '(': ')',
+            '{': '}',
+            '[': ']'
+        }
+
+        self.s = s
+
+        if len(self.s) == 0:
+            return False
+        
+        # initialise a new stack
+        p_stack  = LifoQueue()
+
+        for c in self.s:
+            if c in parentheses.keys():
+                p_stack.put(c)
+            elif c in parentheses.values():
+                # check if the stack is empty
+                if p_stack.empty() == True:
+                    return False
+                elif c != parentheses[p_stack.get()]:
+                    return False
+            else:
+                return False
+        if p_stack.qsize() != 0: # if p stack is not empty, means the brackets were not properly closed
+            return False
+        return True
+                
+s1 = Solution()
+print(s1.isValid("()"))
+print(s1.isValid("()[]{}"))
+print(s1.isValid("(])"))
+print(s1.isValid("(]"))
+print(s1.isValid(""))
+print(s1.isValid("  "))
+print(s1.isValid("["))
